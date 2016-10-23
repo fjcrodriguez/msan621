a04d5f10a299ca2e3d14331d7176b5900035f83e
Francisco Calderon <fcalderonrodriguez@dons.usfca.edu>
2016-10-23 10:05:04 -0700

    need to finsh last feature

--- asign1.py
+++ asign1.py
@@ -4,15 +4,21 @@
 # @date October 21, 2016
 
 import pandas as pd
+import numpy as np
 from sklearn import linear_model
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.preprocessing import Imputer
 
 
 def mse(y, yhat, df):
     return sum( (y - yhat)**2 ) / (len(y) - df)
 
-################## Boston Data #############################
-
+def imputecol(data, value_flag):
+    # requires pandas data frame
+    for i in range(3, data.columns):
+        data.loc[:,i] = data[i].replace(np.mean(data[i].where(data[i] != value_flag)))
+    return data
+# ################# Boston Data #############################
 boston = pd.read_csv('boston.csv')
 
 training_y = boston.iloc[:-50, 13]
@@ -25,12 +31,40 @@ test_x = boston.iloc[-50:, :13]
 reg = linear_model.LinearRegression()
 reg.fit(training_x, training_y)
 pred = reg.predict(test_x)
-print mse(test_y, pred, len(boston.columns)-1)
+print 'Least Squares Regression MSE:', mse(test_y, pred, len(boston.columns)-1)
 
 # KNN Regression on Boston
 k = 10
 knn = KNeighborsRegressor(n_neighbors=k)
 knn.fit(training_x, training_y)
 pred = knn.predict(test_x)
-print mse(test_y, pred, len(boston.columns)-1)
+print 'KNN Regression MSE:', mse(test_y, pred, len(boston.columns)-1)
 
+# ################ Weather Data ##############################
+
+# read in and process temperature columns into integers
+temp = pd.read_csv('hly-temp-normal.txt', header=None, delim_whitespace=True)
+temp[3] = temp[3].str.rstrip('PCS')
+for idx in range(4,27):
+    temp[idx] = temp[idx].str.rstrip('PCS')
+
+# impute missing data
+imputer_col = Imputer(-9999, 'mean', 0)
+for col in range(3,27):
+    temp[col] = pd.to_numeric(temp[col])
+
+temp.loc[:,3:27] = imputer_col.fit_transform(temp.loc[:,3:27])
+
+# separate test data and training data
+training = temp[~temp[0].isin(['USW00023234', 'USW00014918', 'USW00012919', 'USW00013743', 'USW00025309'])]
+test = temp[temp[0].isin(['USW00023234', 'USW00014918', 'USW00012919', 'USW00013743', 'USW00025309'])]
+
+for i in range(3,27):
+    X = pd.DataFrame()
+    nIDs = len(training[0].unique())
+    if i > 3:
+        X['prevhour'] = training[i-1]
+        X['prevday'] = 5# previous day hour
+        X['avg_daily'] = np.mean(training.loc[:, 3:(i - 1)], axis=1)
+
+    X['avg_daily_global'] = np.repeat(training.groupby([1, 2])[i].mean(), nIDs)
